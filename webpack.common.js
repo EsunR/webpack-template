c8fb8fb9f6424b7b270a3d7c29aa05d08add9b14
EsunR <641411169@qq.com>
2019-10-31 11:21:54 +0800

    init

---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Title',
+      template: './asset/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      // 图片加载器
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[hash].[ext]',
+            outputPath: 'images'
+          }
+        }]
+      },
+      // css加载器
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      // scss加载器
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};
