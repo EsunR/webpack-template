a3fe05a21c339fbe1383495b2538c7f39884d604
EsunR <641411169@qq.com>
2019-10-31 15:18:57 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log(document.querySelector("#app"));
+import img from './img/webpack.png';
+
+let app = document.querySelector("#app")
+app.innerHTML = "Hello webpack"
+
+let imgDom = document.createElement("img")
+imgDom.src = img
+imgDom.style.display = "block"
+app.appendChild(imgDom)
