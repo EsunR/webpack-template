33055938d436e95fa0cc822449995cbb171bc8eb
EsunR <641411169@qq.com>
2021-03-18 14:32:07 +0800

    update: 更新最新的包并进行兼容适配

--- webpack.common.js
+++ webpack.common.js
@@ -7,106 +7,86 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin") // 防止样式�
 module.exports = {
   entry: "./src/js/index.ts",
   output: {
-    filename: "bundle.[hash].js",
-    path: path.resolve(__dirname, "dist")
+    filename: "bundle.[fullhash].js",
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Webpack-template",
-      template: "./src/index.html"
+      template: "./src/index.html",
     }),
     new webpack.HotModuleReplacementPlugin(),
     new MiniCssExtractPlugin({
-      filename: "[name].[hash].css",
-      chunkFilename: "[id].[hash].css"
-    })
+      filename: "[name].[fullhash].css",
+      chunkFilename: "[id].[fullhash].css",
+    }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src/")
+      "@": path.resolve(__dirname, "src/"),
     },
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: [".tsx", ".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.(html)$/,
         loader: "html-loader",
-        // options: {
-        //   attributes: {
-        //     list: [
-        //       {
-        //         tag: "img",
-        //         attribute: "src",
-        //         type: "src"
-        //       },
-        //       {
-        //         tag: "img",
-        //         attribute: "pc-src",
-        //         type: "src"
-        //       },
-        //       {
-        //         tag: "img",
-        //         attribute: "mb-src",
-        //         type: "src"
-        //       },
-        //       {
-        //         tag: "link",
-        //         attribute: "href",
-        //         type: "src"
-        //       }
-        //     ]
-        //   }
-        // }
+        options: {
+          sources: {
+            list: [
+              {
+                tag: "img",
+                attribute: "src",
+                type: "src",
+              },
+              {
+                tag: "link",
+                attribute: "href",
+                type: "src",
+              },
+            ],
+          },
+        },
       },
-      // {
-      //   test: /\.(png|svg|jpg|gif)$/,
-      //   use: [
-      //     {
-      //       loader: "file-loader",
-      //       options: {
-      //         name: "[hash].[ext]",
-      //         outputPath: "images"
-      //       }
-      //     }
-      //   ]
-      // },
-      // {
-      //   test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-      //   use: [
-      //     {
-      //       loader: "file-loader",
-      //       options: {
-      //         name: "[name].[ext]",
-      //         outputPath: "fonts"
-      //       }
-      //     }
-      //   ]
-      // },
-      // {
-      //   test: /\.css$/,
-      //   use: [MiniCssExtractPlugin.loader, "css-loader"]
-      // },
-      // {
-      //   test: /\.scss$/,
-      //   use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-      // },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[fullhash][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[fullhash][ext][query]",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
           {
-            loader: "babel-loader"
+            loader: "babel-loader",
           },
-          "eslint-loader"
-        ]
+          "eslint-loader",
+        ],
       },
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "ts-loader", "eslint-loader"]
-      }
-    ]
-  }
+        use: ["babel-loader", "ts-loader", "eslint-loader"],
+      },
+    ],
+  },
 }
